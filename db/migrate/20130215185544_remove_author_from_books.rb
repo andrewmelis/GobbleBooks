8d874781abe7f1ffc604d59fdd5416f4436c5cd6
andrewmelis <amelis@pathf.com>
2013-03-10 21:02:31 -0500

    migration not working

--- db/migrate/20130215185544_remove_author_from_books.rb
+++ db/migrate/20130215185544_remove_author_from_books.rb
@@ -1,5 +1,6 @@
 class RemoveAuthorFromBooks < ActiveRecord::Migration
   def change
-    remove_column :books, :author, :string
+    #remove_column :books, :author, :string
+    remove_column :books, :author
   end
 end
