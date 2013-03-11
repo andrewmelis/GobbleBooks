53484fbaadb7db5aa29c8d2e72b4cd9de644b61c
andrewmelis <amelis@pathf.com>
2013-03-10 21:58:49 -0500

    bcrypt working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :name
+
+  attr_accessible :name, :password
+
+  has_secure_password
+
+  validates_uniqueness_of :name
 
   has_many :favorites
   has_many :books, :through => :favorites
